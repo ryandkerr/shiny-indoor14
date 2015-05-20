cc4ab922682511ae74f7504806172357e6c63a0f
Ryan <ryankerr@college.harvard.edu>
2015-05-20 13:09:50 -0400

    playing with dataset-level filters for assists

--- server.R
+++ server.R
@@ -3,8 +3,16 @@ library(ggplot2)
 
 indoor <- read.csv("data/indoor14.csv")
 
-ast_v_goal <- ggplot(data=indoor, aes(x=Assists, y=Goals)) + geom_text(label=indoor$Player, size=3, angle=-15)
+ast_min <- 5
+ast_max <- 20
+
+selection <- indoor[indoor$Assists > ast_min & indoor$Assists < ast_max,]
+
+ast_v_goal <- ggplot(data=selection, aes(x=Assists, y=Goals)) + geom_text(label=selection$Player, size=3, angle=-15)
 ast_v_goal
+
+head(selection)
+
 
 shinyServer(function(input, output) {
   
