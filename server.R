221a87cca6dbd9dc5d4f1a767eaf1d6996c1e832
Ryan <ryankerr@college.harvard.edu>
2015-05-20 13:36:44 -0400

    added non-functional sliders

--- server.R
+++ server.R
@@ -4,14 +4,12 @@ library(ggplot2)
 indoor <- read.csv("data/indoor14.csv")
 
 ast_min <- 5
-ast_max <- 20
+ast_max <- 50
 
 selection <- indoor[indoor$Assists > ast_min & indoor$Assists < ast_max,]
 
 ast_v_goal <- ggplot(data=selection, aes(x=Assists, y=Goals)) + geom_text(label=selection$Player, size=3, angle=-15)
 ast_v_goal
-
-head(selection)
 
 
 shinyServer(function(input, output) {
--- ui.R
+++ ui.R
@@ -4,9 +4,15 @@ shinyUI(fluidPage(
   titlePanel("Indoor 2014 Data"),
   
   sidebarLayout(
-    sidebarPanel(),
+    sidebarPanel(
+      sliderInput("ast_range", h4("Assists:", align = "center"),
+                  min = 0, max = 40, value = c(0, 40)),
+      
+      sliderInput("goal_range", "Goals:",
+                  min = 0, max = 40, value = c(0, 40))
+    ),
     
-    mainPanel()
+    mainPanel("main panel")
     
   )
 ))
