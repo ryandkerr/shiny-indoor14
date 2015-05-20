cbc13baa1c8279334fab0cde83d9e1a2f9807450
Ryan <ryankerr@college.harvard.edu>
2015-05-20 05:53:10 -0400

    playing with basic ggplot2 scatterplot

--- server.R
+++ server.R
@@ -1,6 +1,10 @@
 library(shiny)
+library(ggplot2)
 
 indoor <- read.csv("data/indoor14.csv")
+
+ast_v_goal <- ggplot(data=indoor, aes(x=Assists, y=Goals)) + geom_text(label=indoor$Player, size=3, angle=-15)
+ast_v_goal
 
 shinyServer(function(input, output) {
   
