9a9523c92825bfceac2ddde7e98fb8b15a85b65e
Ryan <ryankerr@college.harvard.edu>
2015-05-20 15:36:04 -0400

    added games played slider, updated title, added helptext

--- server.R
+++ server.R
@@ -3,6 +3,7 @@ library(ggplot2)
 indoor <- read.csv("data/indoor14.csv")
 
 shinyServer(function(input, output) {
+  
   output$scatter <- renderPlot({
     # min and max assists from slider
     ast_min <- input$ast_range[1]
@@ -12,14 +13,21 @@ shinyServer(function(input, output) {
     goal_min <- input$goal_range[1]
     goal_max <- input$goal_range[2]
     
+    # min and max games played from slider
+    games_min <- input$game_range[1]
+    games_max <- input$game_range[2]
+    
     # filtered data
     selection <- indoor[indoor$Assists >= ast_min &
                         indoor$Assists <= ast_max &
                         indoor$Goals >= goal_min &
-                        indoor$Goals <= goal_max,]
+                        indoor$Goals <= goal_max &
+                        indoor$Games >= games_min &
+                        indoor$Games <= games_max,]
     
     # creating ggplot scatterplot
-    ast_v_goal <- ggplot(data=selection, aes(x=Assists, y=Goals)) + geom_text(label=selection$Player, size=3, angle=-20)
+    ast_v_goal <- ggplot(data=selection, aes(x=Assists, y=Goals)) + 
+      geom_text(label=selection$Player, size=3, angle=-20)
     ast_v_goal
   })
 })
--- ui.R
+++ ui.R
@@ -1,15 +1,20 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Indoor 2014 Data"),
+  titlePanel("11th Anual Jack Marsh Memorial Indoor League"),
   
   sidebarLayout(
     sidebarPanel(
+      helpText("Data from the 2014 Harvard indoor ultimate frisbee league."),
+      
       sliderInput("ast_range", "Assists:",
                   min = 0, max = 40, value = c(0, 40)),
       
       sliderInput("goal_range", "Goals:",
-                  min = 0, max = 40, value = c(0, 40))
+                  min = 0, max = 40, value = c(0, 40)),
+      
+      sliderInput("game_range", "Games Played:",
+                  min = 0, max = 8, value = c(0, 8))
     ),
     
     mainPanel(
